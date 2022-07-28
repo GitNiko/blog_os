843a858cca38576680c90a3146de5c2e9ed689a4
GitNiko <galaxis.ling@gmail>
2022-07-28 17:25:50 +0800

    add test

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,35 @@
 #![no_main]
 
 
-use core::panic::PanicInfo;
-mod vga_buffer;
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+
+use core::panic::PanicInfo;
+use blog_os::{println, print};
+
+
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
     loop {}
 }
 
+#[cfg(test)]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(_info);
+    loop {}
+}
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     print!("some numbers: {} {}", 29, 3.1415926);
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
