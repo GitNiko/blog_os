fa0970458910a557b1ab527bc268f33ec289af6f
GitNiko <galaxis.ling@gmail>
2022-07-28 14:34:56 +0800

    fix mistake

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,12 @@ mod vga_buffer;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    println!("{}", info);
+    println!("{}", _info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
     print!("some numbers: {} {}", 29, 3.1415926);
     loop {}
 }
